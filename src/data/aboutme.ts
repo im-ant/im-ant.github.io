321e3c8821c6167ecd07a5db3aa4c0dade2acdb0
Anthony Chen <26030673+im-ant@users.noreply.github.com>
2025-04-13 13:40:05 +0800

    initial commit, with towaki site format

---
+++ src/data/aboutme.ts
@@ -0,0 +1,37 @@
+export interface AboutMe {
+  name: string;
+  title: string;
+  institution: string;
+  description: string;
+  email: string;
+  imageUrl?: string;
+  blogUrl?: string;
+  cvUrl?: string;
+  googleScholarUrl?: string;
+  twitterUsername?: string;
+  githubUsername?: string;
+  linkedinUsername?: string;
+  funDescription?: string; // Gets placed in the left sidebar
+  secretDescription?: string; // Gets placed in the bottom
+  altName?: string;
+  institutionUrl?: string;
+}
+
+export const aboutMe: AboutMe = {
+  name: "Anthony GX-Chen",
+  title: "Ph.D. Student",
+  institution: "New York University",
+  // Note that links work in the description
+  description: "I am a 4th year PhD student at NYU's <a href='https://wp.nyu.edu/cilvr/'>CILVR Lab</a> and <a href='https://cds.nyu.edu/'>Center for Data Science</a>. My research focuses on better understanding the reinforcement learning (RL) framework, and developing better RL algorithms.<br><br> A few questions I've been thinking about:<br><ul style='list-style-type:disc; margin-left:20px;'><li>How to build agents that efficiently explores and autonomously learns a useful model of the world?</li><li>How to construct abstractions and hierarchies to enable both high-level reasoning and low-level control that is grounded?</li><li>How to design efficient and scalable RL algorithms with a minimal bag of tricks?</li></ul><br> I completed my Master's at Mila / McGill University, co-advised by <a href='https://www.cs.mcgill.ca/~jpineau/'>Prof. Joelle Pineau </a> and <a href='https://sites.google.com/mila.quebec/linc-lab/home'>Prof. Blake Richards</a>. My <a href='https://escholarship.mcgill.ca/concern/theses/2v23vz71g'>master's thesis</a> introduces new ways of decomposing the value function in RL for more efficient learning. This also relates to neuroscientific theories of how the <a href='https://en.wikipedia.org/wiki/Hippocampus'>hippocampus</a> works. <br><br> I was fortunate to work with a number researchers in neuroscience and psychiatry throughout my undergraduate studies: <a href='https://rimuhc.ca/-/yannis-trakadis'>Dr. Yannis Trakadis</a> in psychiatric genomics, <a href='https://www.cobralab.ca/'>Prof. Mallar Chakravarty</a> in computational neuroscience and neuroimaging, and <a href='https://www.fil.ion.ucl.ac.uk/~karl/'>Prof. Karl Friston</a> in theoretical neuroscience. These experiences gave me an appreciation for the tools, knowledge, and perspectives in the neural-sciences.",
+  email: "anthony [DOT] gx [DOT] chen [AT] nyu.edu",
+  imageUrl: "/images/self-IMG-4583_crop.jpg", 
+  googleScholarUrl: "https://scholar.google.com/citations?hl=en&user=7jAlFsIAAAAJ",
+  githubUsername: "im-ant",
+  linkedinUsername: "anthonygxchen",
+  twitterUsername: "AntChen_",
+  // blogUrl: "https://",
+  cvUrl: "https://drive.google.com/file/d/1HmwLkqCvZwMH1_tskRUow2B-FLxdtF62/view?usp=sharing",
+  institutionUrl: "https://wp.nyu.edu/cilvr/",
+  // altName: "",
+  //secretDescription: "I like dogs.",
+};
---
+++ src/data/publication.ts
@@ -0,0 +1,93 @@
+export interface Publication {
+  year: string;
+  conference: string;
+  title: string;
+  authors: string;
+  paperUrl?: string;
+  codeUrl?: string;
+  bibtex?: string;
+  tldr?: string;
+  imageUrl?: string;
+  award?: string;
+  tweetUrl?: string;
+}
+
+export const publicationData: Publication[] = [
+  // If you don't want to show publications, just make the array empty.
+  {
+    year: "2025",
+    conference: "ICLR",
+    title: "Efficient Exploration and Discriminative World Model Learning with an Object-Centric Abstraction",
+    authors: "Anthony GX-Chen, Kenneth Marino, Rob Fergus",
+    paperUrl: "https://arxiv.org/abs/2408.11816",
+    //codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
+    tldr: "Hierarchical reinforcement learning + object centric abstraction + discriminative world model learning. It explore efficiently, plans over long horizons, rapidly solves single tasks, and transfers to different item types and environments.",
+    // imageUrl: "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // award: "🏆 Best Paper Award",
+    // if you have an image in public/images, you can use it like this:
+    imageUrl: "/images/publication/mead-2025-abstract-transition.png"
+  },
+  {
+    year: "2024",
+    conference: "NeurIPS Workshop on Intrinsically Motivated Open-ended Learning",
+    title: "Testing Causal Hypotheses through Hierarchical Reinforcement Learning",
+    authors: "Anthony GX-Chen*, Dongyan Lin*, Mandana Samiei*",
+    paperUrl: "https://openreview.net/forum?id=ZqNcJ8uuHT",
+    tldr: "A framework to think about structural causal models (SCMs) and Markov Decision Processes (MDPs) together, for agentic systems that can test their own causal hypotheses."
+    // codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+  },
+  {
+    year: "2024",
+    conference: "Reinforcement Learning Conference (RLC)",
+    title: "Light-weight probing of unsupervised representations for reinforcement learning",
+    authors: "Wancong Zhang, Anthony GX-Chen, Vlad Sobal, Yann LeCun, Nicolas Carion",
+    paperUrl: "https://arxiv.org/abs/2208.12345",
+    tldr: "We investigate different design choices that makes unsupervised representation learning  work for reinforcement learning, and design a computationally efficient linear probe that correlate strongly with eventual downstream RL performance.",
+    // codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+    imageUrl: "/images/publication/light-weight-rlc-2024-ci.png"
+  },
+  {
+    year: "2022",
+    conference: "AAAI",
+    title: "A Generalized Bootstrap Target for Value-Learning, Efficiently Combining Value and Feature Predictions",
+    authors: "Anthony GX-Chen, Veronica Chelu, Blake Richards, Joelle Pineau",
+    paperUrl: "https://arxiv.org/abs/2201.01836",
+    tldr: "A new way of constructing more efficient one-step bootstrapped learning targets, by combining value estimates (reward prediction) and successor features (feature prediction) in a complementary way. This is a generalization of TD(0), and leads to a spectrum of one-step learning targets trading off value vs. feature predictions.",
+    tweetUrl: "https://x.com/AntChen_/status/1496653952531804161",
+  },
+  {
+    year: "2020",
+    conference: "NeurIPS Workshop in Biological and Artificial Reinforcement Learning",
+    title: "Lambda Successor Return Error",
+    authors: "Anthony GX-Chen, Veronica Chelu, Blake Richards, Joelle Pineau",
+    paperUrl: "publications/gxchen2020_neurips-barl_preprint.pdf",
+    tldr: "We show the value prediction error from lambda-return can be factorized into one-step temporal difference (TD) errors and a successor-like representation (SR). This leads to a new algorithm using SR for error assignment. We show in a tabular setting this results in faster value function learning as compared to both the lambda-return, as well as the (original) SR. We further discuss this perspective in light of the recent neuroscience hypothesis of the brain using successor-like representations.",
+  },
+  {
+    year: "2020",
+    conference: "Frontiers in Artificial Intelligence",
+    title: "A Bayesian Account of Generalist and Specialist Formation Under the Active Inference Framework",
+    authors: "Anthony GX-Chen, David Benrimoh, Thomas Parr, Karl J. Friston",
+    paperUrl: "https://www.frontiersin.org/articles/10.3389/frai.2020.00069/full",
+    tldr: "We model animal / human behaviour using a variational Bayesian (Active Inference) framework. Specifically, we propose how the priors over an agent's policy space can be learned as a result of experience, and how this leads to the phenomenon of specialist and generalist formation. Finally we discuss this in the context of computational psychiatry where symptoms can be explained through faulty inference.",
+  },
+  {
+    year: "2020",
+    conference: "Neuroimage",
+    title: "Investigating microstructural variation in the human hippocampus using non-negative matrix factorization",
+    authors: "Raihaan Patel, Christopher J Steele, Anthony GX-Chen, Sejal Patel, Gabriel A Devenyi, Jürgen Germann, Christine L Tardif, M Mallar Chakravarty",
+    paperUrl: "https://www.sciencedirect.com/science/article/pii/S1053811919309395",
+    tldr: "Using non-negative matrix factorization to discover interpretable components of the human hippocampus from neuroimaging data.",
+    // We investigate the use of an unsupervised statistical method (non-negative matrix factorization) in the analysis of multi-modal neuroimaging dataset. Specifically, we were able to discover from neuroanatomy - in a purely data-driven way - interpretable components of the human hippocampus. We further show interpretable relationships between the components and individual demography and behavioural measures. 
+  },
+  {
+    year: "2019",
+    conference: "American Journal of Medical Genetics Part B: Neuropsychiatric Genetics",
+    title: "Machine learning in schizophrenia genomics, a case‐control study using 5,090 exomes",
+    authors: "Yannis J Trakadis, Sameer Sardaar, Anthony GX-Chen, Vanessa Fulginiti, Ankur Krishnan",
+    paperUrl: "https://onlinelibrary.wiley.com/doi/full/10.1002/ajmg.b.32638",
+    tldr: "Applying machine learning to genetic data for high-accuracy schizophrenia risk prediction and gene feature analysis.",
+    // We apply machine learning to genetic (whole exome sequencing) data, demonstrating our feature extraction and supervised classification pipeline allows for high-accuracy prediction of individuals at high risk for schizophrenia. In addition to providing a clinical tool, we subsequently analyze the top genes (features) utilized by the algorithm in light of existing genetic research, providing new insight into the pathophysiology of schizophrenia.
+  },
+];
