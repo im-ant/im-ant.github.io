c0774f989f11618142b4d00ebc18365b337900bf
Anthony Chen <26030673+im-ant@users.noreply.github.com>
2026-04-11 16:58:13 -0400

    bio updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
                     publicationData.length > 0 && (
                       <section key={sectionName}>
                         <h2 className="font-serif text-l mb-12 tracking-wide uppercase">
-                          Publications
+                          Selected Publication
                         </h2>
                         <div className="space-y-12">
                           {publicationData.map((publication, index) => (
